4e66a98e0c48d601970ce5f11d73982ece26bdcf
SaniLubary <santiago.lp.cop@gmail.com>
2021-07-22 18:43:17 -0300

    funcion goLastPage()

--- utils/funciones.php
+++ utils/funciones.php
@@ -1,9 +1,24 @@
 <?php 
 
-function verEstructura($e){
+function verEstructura($e) {
     echo "<pre>";
     print_r($e);
     echo "</pre>"; 
+}
+
+function goLastPage() {
+    global $PROYECTO;
+    
+    $host = $_SESSION['HTTP_HOST'];
+    $port = $_SESSION['SERVER_PORT'];
+    
+    if(isset($_SESSION['url'])) 
+        $url = $_SESSION['url'];
+    else 
+        $url = "$PROYECTO/vista/index.php";
+
+    header("Location: http://$host:$port/$url");
+
 }
 
 function __autoload($class_name){
